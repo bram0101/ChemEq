1d1b118cf17fc06be332340972b6139fdc36fa48
bram0101 <bram.stout@home.nl>
2017-10-03 13:20:56 +0200

    Finished methods for parser
    Parser has the methods, just needs to be bug fixed.
    
    Also added the licenses to the files.

--- src/me/bramstout/chemeq/InputDisplay.java
+++ src/me/bramstout/chemeq/InputDisplay.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2017 Bram Stout
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package me.bramstout.chemeq;
 
 import javafx.beans.value.ChangeListener;
@@ -15,7 +39,11 @@ import javafx.scene.paint.Color;
 
 public class InputDisplay extends HBox{
 	
+	private Parser parser;
+	
 	public InputDisplay(TextField inputTextField) {
+		parser = new Parser();
+		
 		minWidthProperty().bind(inputTextField.widthProperty());
 		minHeightProperty().bind(inputTextField.heightProperty());
 		maxWidthProperty().bind(inputTextField.widthProperty());
@@ -44,28 +72,9 @@ public class InputDisplay extends HBox{
 	
 	public void parse(String value) {
 		getChildren().clear();
-		int index = 0;
-		while(index < value.length()) {
-			index = parseMolecule(value, index);
-			index++;
-		}
-	}
-	
-	public int parseMolecule(String value, int index) {
-		StringBuilder builder = new StringBuilder();
-		char firstChar = value.charAt(index);
-		char c = firstChar;
-		while(true) {
-			builder.append(c);
-			index++;
-			if(index >= value.length())
-				break;
-			c = value.charAt(index);
-			if((!Character.isLetter(c) && !Character.isDigit(c)))
-				break;
-		}
-		getChildren().add(new Label(builder.toString()));
-		return index;
+		
+		Reaction reaction = parser.parse(value);
+		getChildren().add(new Label(reaction.toString()));
 	}
 
 }
--- src/me/bramstout/chemeq/Parser.java
+++ src/me/bramstout/chemeq/Parser.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ * 
+ * Copyright (c) 2017 Bram Stout
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package me.bramstout.chemeq;
 
 import java.util.ArrayList;
@@ -6,22 +30,89 @@ import java.util.List;
 public class Parser {
 
 	public Reaction parse(String s) {
-		StringIterator si = new StringIterator(s);
+		StringIterator si = new StringIterator(" " + s.trim().replace('\t', ' '));
+
 		List<Molecule> leftTerm = parseTerm(si);
+
+		while (si.hasNext() && !Character.isWhitespace(si.peekNext()))
+			si.skip();
+		si.skipSpaces();
+
 		List<Molecule> rightTerm = parseTerm(si);
 		List<Element> elements = new ArrayList<Element>();
+
 		// TODO: Go through the terms, and collect the elements
+
 		return new Reaction(leftTerm, rightTerm, elements);
 	}
 
 	private List<Molecule> parseTerm(StringIterator si) {
 		List<Molecule> term = new ArrayList<Molecule>();
-		while(si.hasNext()) {
+
+		while (si.hasNext()) {
+			si.skipSpaces();
 			char c = si.peekNext();
-			if(c == '=' || c == '-')
+			if (c == '=' || c == '-')
+				break;
+			if (c == '+') {
+				si.skip();
+				si.skipSpaces();
+			}
+			term.add(parseMolecule(si));
+		}
+
+		return term;
+	}
+
+	private Molecule parseMolecule(StringIterator si) {
+		List<Element> elements = new ArrayList<Element>();
+		int factor = 0;
+
+		if (!si.hasNext())
+			return new Molecule(elements, factor);
+
+		if (Character.isDigit(si.peekNext())) {
+			StringBuilder sb = new StringBuilder();
+			while (si.hasNext() && Character.isDigit(si.peekNext())) {
+				sb.append(si.next());
+			}
+			factor = Integer.parseInt(sb.toString());
+		}
+
+		while (si.hasNext()) {
+			if (si.peekNext() == '(') {
+				si.skip();
+				elements.add(parseMolecule(si));
+				if (si.hasNext() && si.peekNext() != ')')
+					System.err.println("Err: inline molecule has no closure. Missing ')' at " + si.getIndex()
+							+ ", found character '" + si.peekNext() + "'");
+				si.skip();
+			} else if (Character.isLetter(si.peekNext())) {
+				elements.add(parseElement(si));
+			} else {
+				break;
+			}
+		}
+
+		return new Molecule(elements, factor);
+	}
+
+	private Element parseElement(StringIterator si) {
+		StringBuilder sb = new StringBuilder();
+		int factor = 0;
+		while (si.hasNext()) {
+			sb.append(si.next());
+			if (si.hasNext() && (Character.isDigit(si.peekNext()) || Character.isUpperCase(si.peekNext())))
 				break;
 		}
-		return term;
+		if(si.hasNext() && Character.isDigit(si.peekNext())) {
+			StringBuilder sb2 = new StringBuilder();
+			while (si.hasNext() && Character.isDigit(si.peekNext())) {
+				sb2.append(si.next());
+			}
+			factor = Integer.parseInt(sb2.toString());
+		}
+		return new Element(sb.toString(), factor);
 	}
 
 }
